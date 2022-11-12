1affe390c0b495276f497b8b0d6e33291464a63e
Denis <denis.musoev@yandex.ru>
2022-11-13 01:17:59 +0400

    готовая 4 лаба

--- modal.js
+++
@@ -1,4 +0,0 @@
-addFilm.onclick = function () {
-    var imglem = document.getElementById('js-input-image').innerHTML = imagefilms;
-    var namelem = document.getElementById('js-input-image').value;
-}
